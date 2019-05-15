9015f159174fc4a6816bd1ec7d76c7fdd32a0df6
princeYang <38146875+dizhenhuoshan@users.noreply.github.com>
2019-05-16 00:29:45 +0800

    final (sort out)

--- src/princeYang/mxcc/Config.java
+++ src/princeYang/mxcc/Config.java
@@ -3,7 +3,7 @@ package princeYang.mxcc;
 public class Config
 {
     static public  int regSize = 8;
-    static public  int INLINE_MAX_INST = 256;
+    static public  int INLINE_MAX_INST = 69;
     static public  int INLINE_FUNC_INST = 6000;
     static public  int INLINE_MAX_DEPTH = 8;
 }
